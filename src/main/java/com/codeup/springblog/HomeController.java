bfbb8842eb4e1125bef6b5ee6b5718faa7ede38b
AugustineCervantes <Augustine.CervantesIII@gmail.com>
2020-07-31 15:14:41 -0500

    adding views lecture and mini exercise

--- src/main/java/com/codeup/springblog/HomeController.java
+++ src/main/java/com/codeup/springblog/HomeController.java
@@ -1,16 +1,28 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
 
+//    @GetMapping("/")
+//    @ResponseBody
+//    public String page(){
+//        return"this is the landing page.";
+//    }
+
     @GetMapping("/")
-    @ResponseBody
+// responsebody removed to connect a controller to a view on home.html file
     public String page(){
-        return"this is the landing page.";
+        return "home";
+    }
+
+    @PostMapping("/")
+    public String returnCohort(@RequestParam(name = "cohort") String cohort, Model model){
+        model.addAttribute("cohort", cohort);
+        return "home";
     }
 
 }
---
+++ src/main/java/com/codeup/springblog/LoginController.java
@@ -0,0 +1,25 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String viewLoginForm(){
+        //html form here
+        return "login";
+    }
+
+    @PostMapping("/profile")
+    public String viewLoginInfo(@RequestParam(name = "username") String username, @RequestParam(name = "password") String password, Model model){
+        model.addAttribute("username", username);
+        model.addAttribute("password", password);
+        return "profile";
+    }
+
+}
